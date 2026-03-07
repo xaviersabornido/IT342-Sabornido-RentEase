8e8c82a2dc8fd8586a2f915ffa16c97c3771a39f
LAB13_USER ON WS21 <L13Y09W21@LabHCI.net>
2026-03-07 08:26:20 +0800

    Add Spring Boot backend application

---
+++ backend/src/main/java/edu/cit/sabornido/rentease/RenteaseApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.sabornido.rentease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenteaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenteaseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/edu/cit/sabornido/rentease/RenteaseApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.sabornido.rentease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenteaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
